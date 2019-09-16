54753cbc08c8fcf74052978604349122cd46abe7
jesusaugusto0x2 <jesusaugusto.008@gmail.com>
2019-09-16 07:35:52 -0400

    modificacion de nodo identificador para las declaraciones

--- src/ast/NodoIdentificador.java
+++ src/ast/NodoIdentificador.java
@@ -1,19 +1,34 @@
 package ast;
 
 public class NodoIdentificador extends NodoBase {
-	private String nombre;
+  private String nombre;
+  private String tipo;
 
-	public NodoIdentificador(String nombre) {
-		super();
-		this.nombre = nombre;
-	}
+  public NodoIdentificador(String nombre) {
+    super();
+    this.nombre = nombre;
+    this.tipo = "no_asignado";
+  }
+  
+  public NodoIdentificador (String nombre, String tipo) {
+    super();
+    this.nombre = nombre;
+    this.tipo = tipo;
+  }
 
-	public NodoIdentificador() {
-		super();
-	}
+  public NodoIdentificador() {
+    super();
+  }
 
-	public String getNombre() {
-		return nombre;
-	}
-
+  public String getNombre() {
+    return nombre;
+  }
+  
+  public String getTipo () {
+    return this.tipo;
+  }
+  
+  public void setTipo (String tipo) {
+    this.tipo = tipo;
+  }
 }
